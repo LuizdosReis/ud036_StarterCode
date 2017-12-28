bd2f6a89da828916153777026219806fc9f517a8
luiz dos reis <luizdosreis@gmail.com>
2017-12-28 19:47:40 -0200

    revisão

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 # ud036_StarterCode
-Source code for a Movie Trailer website.
 
+Trailer of Movies
+
+How It Works
+Create an page with movies
+
+
+Usage
 To run this code do you should have Python 2.7 install in your computer.
 
 After run entertainment_center.py and your browser will open the page of movies.
--- media.py
+++ media.py
@@ -2,14 +2,27 @@ import webbrowser
 
 
 class Movie:
+    """ This class create the objects of movies """
 
-    VALID_RATINGS = ['G', 'PG', 'PG-13', 'R']
+    VALID_RATINGS = ['G',
+                     'PG',
+                     'PG-13',
+                     'R',
+                     ]
 
-    def __init__(self, movie_title, movie_storyline, movie_post_image, movie_trailer_youtube_url):
+    def __init__(self,
+                 movie_title,
+                 movie_storyline,
+                 movie_post_image,
+                 movie_trailer_youtube_url,
+                 ):
+        """ This constructor initializes all attributes of class """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = movie_post_image
         self.trailer_youtube_url = movie_trailer_youtube_url
 
-    def show_trailer(self):
+    def show_trailer(self,
+                     ):
+        """ This method open browser in the trailer of movie"""
         webbrowser.open(self.trailer_youtube_url)
